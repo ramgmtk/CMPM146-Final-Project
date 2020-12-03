9d77a6568bc0f6f7dc0a9f79bf2f9912f9d090b5
Emmanuel Butor <ebutor@ucsc.edu>
2020-12-03 00:05:00 -0800

    Added some basic stuff for the DramaManager, like UpdateDramaManager() function that keeps dramaMeter to a certain maximum number. Also room traversal will update dramaMeter using that function. Additionally, in RoomDirector.cs, I made the AddElementRandomly a public function.

--- 2dRogueLike/Assets/Scripts/DramaManager.cs
+++ 2dRogueLike/Assets/Scripts/DramaManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DramaManager : MonoBehaviour
 {
     public float dramaMeter;
+    public const float MAX_DRAMAMETER = 100;
     private RoomDirector rd;
     private PlayerPlanner pp;
     // Start is called before the first frame update
@@ -28,6 +29,9 @@ public class DramaManager : MonoBehaviour
     //Drama management should be done before the room update is called.
     public void UpdateRoom(ref GameObject room, ref PlayerStats player)
     {
+        Debug.Log(string.Format(
+            "Before Room Visit; PlayerStats = {0}HP, {1} keys; dramaMeter at {2}",
+            player.health, player.keyCount, dramaMeter));
         GameObject[,] currentElements = room.GetComponent<Room>().elements;
         if (room.GetComponent<Room>().locked)
         {
@@ -40,9 +44,11 @@ public class DramaManager : MonoBehaviour
             {
                 if (currentElements[i,j])
                 {
+                    // UpdateDramaMeter(float) used to update dramaMeter
                     if (currentElements[i, j].tag == "Enemy")
                     {
                         player.health = player.health - pp.enemyDamage;
+                        UpdateDramaMeter((float)(100 - player.health) / 10);
                     }
                     else if (currentElements[i, j].tag == "Key")
                     {
@@ -51,16 +57,87 @@ public class DramaManager : MonoBehaviour
                     else if (currentElements[i, j].tag == "Food")
                     {
                         player.health +=  5;
+                        UpdateDramaMeter(-5);
                     }
                     rd.EditElementInRoom(ref room, i, j, null);
                 }
             }
         }
+        Debug.Log(string.Format(
+            "After Room Visit; PlayerStats = {0}HP, {1} keys; dramaMeter at {2}",
+            player.health, player.keyCount, dramaMeter));
     }
 
     public void DramatizeRoom(ref GameObject room, ref PlayerStats player)
     {
         //Fill in code here
+        System.Random rng = new System.Random();
+        /** Phases for drama
+         * Increasing Intensity
+         *  - Add more enemies in next room
+         * Relaxation
+         *  - Add Food and fewer/no enemies
+        */
+        // Increasing Intensity Phase - Add more enemies
+        if (dramaMeter < 40)
+		{
+            // Set # of enemies to spawn from 0 to dramaMeter
+            int numEnemyCount = rng.Next((int)dramaMeter);
+            // Clamp this number so that we don't overgenerate too many enemies
+            const int UPPER_ENEMY_LIMIT = 8;
+            if (numEnemyCount > UPPER_ENEMY_LIMIT)
+			{
+                numEnemyCount = UPPER_ENEMY_LIMIT;
+			}
+            // Spawn Enemies
+            for (int i = 0; i < numEnemyCount; i++)
+			{
+                rd.AddElementRandomly(room, rd.enemy);
+			}
+            Debug.Log(string.Format("Enemies added: {0}", numEnemyCount));
+		}
+        else
+		{
+            // Check player HP to decide how much food to provide
+            int numFoodCount = 0;
+            if (player.health < 25)
+			{
+                numFoodCount = 10;
+			}
+            else if (player.health < 50)
+			{
+                numFoodCount = 5;
+			}
+            else if (player.health < 75)
+			{
+                numFoodCount = 3;
+			}
+            else
+			{
+                numFoodCount = rng.Next(2); // either 1 or 0 food
+			}
+            for (int i = 0; i < numFoodCount; i++)
+			{
+                rd.AddElementRandomly(room, rd.food);
+			}
+            Debug.Log(string.Format("Food added: {0}", numFoodCount));
+		}
+        
         return;
     }
+
+    /// <summary>
+	/// Updates the dramaMeter field, restricting it to
+	/// MAX_DRAMAMETER
+	/// </summary>
+	/// <param name="delta">how much to change (additive)</param>
+    public void UpdateDramaMeter(float delta = 0)
+	{
+        float newDramaMeter = dramaMeter + delta;
+        if (newDramaMeter > MAX_DRAMAMETER)
+		{
+            newDramaMeter = MAX_DRAMAMETER;
+		}
+        dramaMeter = newDramaMeter;
+	}
 }
